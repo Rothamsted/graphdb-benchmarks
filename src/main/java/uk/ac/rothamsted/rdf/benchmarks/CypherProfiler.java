2336c48b1ae71b5d0ff04217fcaa6a403f268fef
Marco Brandizi <marco.brandizi@gmail.com>
2021-10-18 18:04:32 +0100

    Merging Carlos' code (Gremlin and updates).

--- src/main/java/uk/ac/rothamsted/rdf/benchmarks/CypherProfiler.java
+++ src/main/java/uk/ac/rothamsted/rdf/benchmarks/CypherProfiler.java
@@ -3,7 +3,6 @@ package uk.ac.rothamsted.rdf.benchmarks;
 import org.neo4j.driver.AccessMode;
 import org.neo4j.driver.AuthToken;
 import org.neo4j.driver.AuthTokens;
-import org.neo4j.driver.Config;
 import org.neo4j.driver.Driver;
 import org.neo4j.driver.GraphDatabase;
 import org.neo4j.driver.Session;
