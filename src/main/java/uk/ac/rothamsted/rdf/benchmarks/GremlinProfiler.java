a4955bbc963128aded9f3e87a95889c2d1497e26
Marco Brandizi <marco.brandizi@gmail.com>
2023-03-28 13:42:49 +0100

    Cleaning and improving GremlinProfile.

--- src/main/java/uk/ac/rothamsted/rdf/benchmarks/GremlinProfiler.java
+++ src/main/java/uk/ac/rothamsted/rdf/benchmarks/GremlinProfiler.java
@@ -2,6 +2,7 @@ package uk.ac.rothamsted.rdf.benchmarks;
 
 import static java.lang.System.out;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
@@ -12,144 +13,134 @@ import org.apache.tinkerpop.gremlin.driver.RequestOptions;
 import org.apache.tinkerpop.gremlin.driver.ser.GryoMessageSerializerV3d0;
 import org.apache.tinkerpop.gremlin.structure.io.gryo.GryoMapper;
 import org.janusgraph.graphdb.tinkerpop.JanusGraphIoRegistry;
+
+import com.machinezoo.noexception.Exceptions;
+
 import org.apache.tinkerpop.gremlin.driver.Result;
 
+import uk.ac.ebi.utils.exceptions.ExceptionUtils;
 import uk.ac.ebi.utils.time.XStopWatch;
 
 /**
- * The Gremlin profiler. 
+ * The Gremlin profiler.
  * 
- * Important kudos to Paco Cañas and Ramón Arellano, with whom I passed quite a time playing with tinkerpop/janusgraph and 
- * its configuration. 
+ * Important kudos to Paco Cañas and Ramón Arellano, with whom I passed quite a time playing with tinkerpop/janusgraph
+ * and its configuration.
  *
  * @author cbobed
- * <dl><dt>Date:</dt><dd>25 May 2021</dd></dl>
+ *         <dl>
+ *         <dt>Date:</dt>
+ *         <dd>25 May 2021</dd>
+ *         </dl>
  *
  */
 public class GremlinProfiler extends AbstractProfiler
 {
-	
-	/* some configuration parameters that we have found useful via experimentation */ 
-	public static final int MIN_CONNECTION_POOL_SIZE = 2; 
-    public static final int MAX_CONNECTION_POOL_SIZE = 8; 
-    public static final int MIN_USAGES_PER_CONNECTION = 8; 
-    public static final int MAX_USAGES_PER_CONNECTION = 16; 
-    public static final int MIN_IN_PROCESS_PER_CONNECTION = 1; 
-    public static final int MAX_IN_PROCESS_PER_CONNECTION = 4; 
-    public static final int MAX_CONTENT_LENGTH = 65536000; 
-    public static final int MAX_WAIT_FOR_CONNECTION = 30000; 
-    public static final int RESULT_ITERATION_BATCH_SIZE = 64; 
+	/** some configuration parameters that we have found useful via experimentation */
+	public static final int MIN_CONNECTION_POOL_SIZE = 2,
+		MAX_CONNECTION_POOL_SIZE = 8,
+		MIN_USAGES_PER_CONNECTION = 8,
+		MAX_USAGES_PER_CONNECTION = 16,
+		MIN_IN_PROCESS_PER_CONNECTION = 1,
+		MAX_IN_PROCESS_PER_CONNECTION = 4,
+		MAX_CONTENT_LENGTH = 65536000,
+		MAX_WAIT_FOR_CONNECTION = 30000,
+		RESULT_ITERATION_BATCH_SIZE = 64;
 
-    /* given the problems with executions in gremlin we count the potentials timeouts*/ 
-    protected int numberOfTimeouts = 0; 
-    
-	protected Cluster cluster;
-	protected Client client;
-	
-	private static GryoMapper.Builder builder = null; 
-	private static MessageSerializer serializer = null; 
+	/** given the problems with executions in gremlin we count the potentials timeouts */
+	private int numberOfTimeouts = 0;
 
-	protected Integer port; 
-		
-	public GremlinProfiler ( String basePath, String endPointUrl, Integer port)
+	private Cluster cluster;
+
+	private Integer port;
+
+	public GremlinProfiler ( String basePath, String endPointUrl, Integer port )
 	{
 		this ();
-		this.basePath = basePath; 
+		this.basePath = basePath;
 		this.endPointUrl = endPointUrl;
-		this.port = port; 
+		this.port = port;
 	}
 
 	public GremlinProfiler ()
 	{
 		super ( "gremlin" );
 	}
-	
+
 	@Override
 	protected long profileQuery ( String name )
 	{
 		String gremlinQuery = getQueryString ( name );
 		
-		builder = GryoMapper.build().addRegistry(JanusGraphIoRegistry.getInstance());
-	    serializer = new GryoMessageSerializerV3d0(builder);
-	    if (cluster == null) { 
-	    	// we build another cluster also if the one is already closed or is already closing
-	    	cluster = Cluster.build(this.endPointUrl).port(this.port)
-                .serializer(serializer)
-                .minConnectionPoolSize(MIN_CONNECTION_POOL_SIZE)
-                .maxConnectionPoolSize(MAX_CONNECTION_POOL_SIZE)
-                .minSimultaneousUsagePerConnection(MIN_USAGES_PER_CONNECTION)
-                .maxSimultaneousUsagePerConnection(MAX_USAGES_PER_CONNECTION)
-                .minInProcessPerConnection(MIN_IN_PROCESS_PER_CONNECTION)
-                .maxInProcessPerConnection(MAX_IN_PROCESS_PER_CONNECTION)
-                .maxContentLength(MAX_CONTENT_LENGTH)
-                .maxWaitForConnection(MAX_WAIT_FOR_CONNECTION)
-                .resultIterationBatchSize(RESULT_ITERATION_BATCH_SIZE).create();
-	    }
-	    else {
-	    	if  (cluster.isClosed() || cluster.isClosing() ) { 
-	 
-	    	// we build another cluster also if the one is already closed or is already closing
-	    	cluster = Cluster.build(this.endPointUrl).port(this.port)
-                .serializer(serializer)
-                .minConnectionPoolSize(MIN_CONNECTION_POOL_SIZE)
-                .maxConnectionPoolSize(MAX_CONNECTION_POOL_SIZE)
-                .minSimultaneousUsagePerConnection(MIN_USAGES_PER_CONNECTION)
-                .maxSimultaneousUsagePerConnection(MAX_USAGES_PER_CONNECTION)
-                .minInProcessPerConnection(MIN_IN_PROCESS_PER_CONNECTION)
-                .maxInProcessPerConnection(MAX_IN_PROCESS_PER_CONNECTION)
-                .maxContentLength(MAX_CONTENT_LENGTH)
-                .maxWaitForConnection(MAX_WAIT_FOR_CONNECTION)
-                .resultIterationBatchSize(RESULT_ITERATION_BATCH_SIZE).create();
-    		}
-	    }
-	  
-		try{
+		if ( cluster == null || cluster.isClosed () || cluster.isClosing () )
+		{
+			// we build another cluster also if the one is already closed or is already closing
+			//
 			
-			client = cluster.connect();
+			GryoMapper.Builder builder = GryoMapper.build ()
+				.addRegistry ( JanusGraphIoRegistry.getInstance () );
+			MessageSerializer serializer = new GryoMessageSerializerV3d0 ( builder );
+			
+			cluster = Cluster.build ( this.endPointUrl )
+				.port ( this.port )
+				.serializer ( serializer )
+				.minConnectionPoolSize ( MIN_CONNECTION_POOL_SIZE ).maxConnectionPoolSize ( MAX_CONNECTION_POOL_SIZE )
+				.minSimultaneousUsagePerConnection ( MIN_USAGES_PER_CONNECTION )
+				.maxSimultaneousUsagePerConnection ( MAX_USAGES_PER_CONNECTION )
+				.minInProcessPerConnection ( MIN_IN_PROCESS_PER_CONNECTION )
+				.maxInProcessPerConnection ( MAX_IN_PROCESS_PER_CONNECTION ).maxContentLength ( MAX_CONTENT_LENGTH )
+				.maxWaitForConnection ( MAX_WAIT_FOR_CONNECTION ).resultIterationBatchSize ( RESULT_ITERATION_BATCH_SIZE )
+				.create ();
+		} 
+
+		Client client = null;
+		try
+		{
+			client = cluster.connect ();
+			final Client clientRO = client; // required in the lambda
+
+			RequestOptions options = RequestOptions.build ()
+				.timeout ( 500 )
+				.create ();
+			
 			return XStopWatch.profile ( () -> 
 			{
-				
-				 try {
-//					 List<Result> list;
-//					 list = client.submit(gremlinQuery).all().get();
-//					 
-					 // this is the setup
-					 RequestOptions options = RequestOptions.build().timeout(500).create();
-					 List<Result> list = client.submit(gremlinQuery, options).all().get();
-					 					 
-			        } 
-				 	catch (ExecutionException ex) {
-				 		ex.printStackTrace();
-				 		this.numberOfTimeouts ++; 
-				 	}
-				 	catch (Exception ex) {
-			            ex.printStackTrace();
-			        }
-				client.close(); 
-				 
+				try
+				{
+					List<Result> list = clientRO.submit ( gremlinQuery, options ).all ().get ();
+					// Force fetching of the first result, just in case it's lazy
+					Iterator<Result> itr = list.iterator ();
+					if ( itr.hasNext () ) itr.next ();
+				}
+				catch ( ExecutionException ex )
+				{
+					this.numberOfTimeouts++;
+					log.debug ( "timeout for Gremlin query '{}'", name );
+				}
+				catch ( Exception ex )
+				{
+					ExceptionUtils.throwEx ( 
+						RuntimeException.class, ex, 
+						"Error while running Gremlin query '%s': $cause, query is:\n%s\n", name, gremlinQuery
+					);
+				}
 			});
-			
 		}
-		catch ( Exception ex ) 
+		finally
 		{
-			log.error ( "Error while executing {}, query is:\n{}", name, gremlinQuery);
-			throw new IllegalArgumentException ( "Error while executing Gremlin'" + name + "': " + ex.getMessage (), ex ); 
-		}
-		finally {
-			client.close(); // client does not implement autocloseable interface
+			if ( client != null ) client.close (); // client does not implement auto-closeable interface
 		}
 	}
 
-
-
-	public int getNumberOfTimeouts() {
+	public int getNumberOfTimeouts ()
+	{
 		return numberOfTimeouts;
-	}	
-	
+	}
+
 	public void profile ( int repeats )
 	{
-		super.profile(repeats); 
-		out.println("----"); 
-		out.println("Number of timeouts: "+this.numberOfTimeouts); 
-	}	
+		super.profile ( repeats );
+		out.println ( "----" );
+		out.println ( "Number of timeouts: " + this.numberOfTimeouts );
+	}
 }
