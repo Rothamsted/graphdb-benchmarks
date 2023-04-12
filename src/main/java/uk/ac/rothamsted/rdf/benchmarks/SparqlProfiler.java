b768baba15e13a1325d1ab85e684d7e92ace7ee7
Marco Brandizi <marco.brandizi@gmail.com>
2023-04-12 13:55:47 +0100

    SPARQL profiler, workaround for the new way of managing non-consumed results.

--- src/main/java/uk/ac/rothamsted/rdf/benchmarks/SparqlProfiler.java
+++ src/main/java/uk/ac/rothamsted/rdf/benchmarks/SparqlProfiler.java
@@ -41,18 +41,25 @@ public class SparqlProfiler extends AbstractProfiler
 		if ( this.rdfConnection == null )
 			this.rdfConnection = RDFConnection.connect ( endPointUrl );
 		
+		XStopWatch watch = new XStopWatch ();
+		
 		// Clock the query
 		try
 		{
-			return XStopWatch.profile ( () -> 
+			watch.start ();
+			Query query = QueryFactory.create ( sparql, Syntax.syntaxARQ );
+			try ( QueryExecution qx = this.rdfConnection.query ( query ) )
 			{
-				Query query = QueryFactory.create ( sparql, Syntax.syntaxARQ );
-				try ( QueryExecution qx = this.rdfConnection.query ( query ) )
+				// According to the most recent Jena, result streams are always consumed when reusing the 
+				// connections. Because the latter is typical, we are going to consume everything, but
+				// we're timing the first fetch only, as this is what we want to test and compare.
+				for ( ResultSet rs = qx.execSelect (); rs.hasNext (); )
 				{
-					ResultSet rs = qx.execSelect ();
-					if ( rs.hasNext () ) rs.next ();
+					rs.next ();
+					if ( watch.isStarted () ) watch.stop ();
 				}
-			});
+			}
+			return watch.getTime ();
 		}
 		catch ( QueryException ex ) 
 		{
