c7f4465aac73cbcf0a0e2e75c24ae50ff5bbe4cf
Marco Brandizi <marco.brandizi@gmail.com>
2023-07-20 18:09:28 +0100

    Minor fixes to Arcade test class.

--- src/test/java/uk/ac/rothamsted/rdf/benchmarks/ArcadeProfilerTest.java
+++ src/test/java/uk/ac/rothamsted/rdf/benchmarks/ArcadeProfilerTest.java
@@ -14,7 +14,10 @@ public class ArcadeProfilerTest
 	@Test
 	public void testBasics ()
 	{
-		var profiler = new ArcadeProfiler ( "/tmp/queries/", "biopax", "TestTest" );
+		var queriesPath = "src/main/assembly/resources/gremlin/";
+		// DEBUG queriesPath = "/tmp/queries/";
+		// NOTE: don't store multiple DB in the same server instance, we do not know if this affects performance.
+		var profiler = new ArcadeProfiler ( queriesPath, "biopax", "TestTest" );
 		profiler.profile ( 10 );
 	}
 }
